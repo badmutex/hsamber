414bfef9509dc0735367aeec50234de2b7283e80
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2009-12-04 21:48:55 -0500

    formatting, comments

--- Data/Biophys/Topology/Amber/Types.hs
+++ Data/Biophys/Topology/Amber/Types.hs
@@ -7,13 +7,14 @@ module Data.Biophys.Topology.Amber.Types where
 
 
 -- | i.e. amber99_17 is H bonded to nitrogen atoms
-newtype AtomType = AtomType String deriving (Eq, Ord, Show)
+newtype AtomType    = AtomType    String deriving (Eq, Ord, Show)
 -- | i.e. LEU
 newtype ResidueName = ResidueName String deriving (Eq, Ord, Show)
 -- | i.e. N, H1, CE, etc
-newtype AtomName = AtomName String deriving (Eq, Ord, Show)
+newtype AtomName    = AtomName    String deriving (Eq, Ord, Show)
 
 -- -------------------- Units -------------------- --
+-- | Show the type 't' where 'a' is a phantom type
 class TShow t where tshow :: t a -> String
 
 data AMU
@@ -28,10 +29,10 @@ instance TShow t => Show (t KiloJoulesPerMol) where show = tshow' "kJ/mol"
 instance TShow t => Show (t NanoMeters)       where show = tshow' "nm"
 
 -- -------------------- Dimensions -------------------- --
-data Mass a = Mass Double
-data Charge a = Charge Double
+data Mass a    = Mass Double
+data Charge a  = Charge Double
 data Epsilon a = Epsilon Double
-data Sigma a = Sigma Double
+data Sigma a   = Sigma Double
 
 instance TShow Mass    where tshow (Mass m)    = "Mass "    ++ show m
 instance TShow Charge  where tshow (Charge c)  = "Charge "  ++ show c
