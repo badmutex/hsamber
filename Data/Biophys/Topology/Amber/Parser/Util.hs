373b0d91fac5f339d701b3017027da6433e65fc2
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2009-12-05 13:28:22 -0500

    utility parsers

---
+++ Data/Biophys/Topology/Amber/Parser/Util.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE
+  FlexibleInstances
+  , TypeSynonymInstances
+  #-}
+
+module Data.Biophys.Topology.Amber.Parser.Util where
+
+import Data.Monoid
+import Text.ParserCombinators.Parsec
+
+
+-- | Platform (Windows, *nix) agnostic line terminator
+eol :: Parser String
+eol = try (string "\n\r")
+      <|> try (string "\r\n")
+      <|> string "\n"
+      <|> string "\r"
+      <?> "end of line"
+
+
+instance Monoid (Parser String) where
+    mempty = option "" (many anyChar)
+    mappend p1 p2 = do
+      p1' <- p1
+      p2' <- p2
+      return $ p1' ++ p2'
+ 
+-- | Parser concatenation
+(<++>) :: (Monoid a) => a -> a -> a
+l <++> r = mconcat [l,r]
+ 
+
+number :: Parser String
+number = many digit
+
+positiveInt, negativeInt :: (Integral i, Read i) => Parser i
+positiveInt = read `fmap` number
+negativeInt = read `fmap` (option "" (string "-") <++> number)
+ 
+-- | Parse (-/+) integers
+integral :: (Integral i, Read i) => Parser i
+integral = negativeInt <|> positiveInt
+ 
+ 
+-- | Parse reals
+decimal :: (Read f, Fractional f) => Parser f
+decimal = do
+  ds <- many digit
+  char '.'
+  ds' <- many digit
+  return . read $ ds ++ "." ++ ds'
+  <?> "a fractional number"
