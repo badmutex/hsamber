af3d4c592bb509bcb283a7de6802a9bc994e59ee
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2009-12-05 13:47:37 -0500

    names changed for clarity in src reading

--- Data/Biophys/Topology/Amber/Parser/Defaults.hs
+++ Data/Biophys/Topology/Amber/Parser/Defaults.hs
@@ -7,32 +7,32 @@ import Data.Biophys.Topology.Amber.Parser.Util
 import Text.ParserCombinators.Parsec hiding (newline)
 
 
-defaults' :: Parser ()
-defaults' = do char '['          ; spaces
-               string "defaults" ; spaces
-               char ']'
-               space `manyTill` eol
-               return ()
+header :: Parser ()
+header = do char '['          ; spaces
+            string "defaults" ; spaces
+            char ']'
+            space `manyTill` eol
+            return ()
 
 gen_pairs "yes" = True
 gen_pairs "no"  = False
 
-defaults'' :: Parser Defaults
-defaults'' = do nbfunc   <- positiveInt                                           ; spaces
-                combrule <- positiveInt                                           ; spaces
-                genpairs <- gen_pairs `fmap` (try (string "yes") <|> string "no") ; spaces
-                fLJ      <- decimal                                               ; spaces
-                fQQ      <- decimal
-                space `manyTill` (try comment <|> eol)
+values :: Parser Defaults
+values = do nbfunc   <- positiveInt                                           ; spaces
+            combrule <- positiveInt                                           ; spaces
+            genpairs <- gen_pairs `fmap` (try (string "yes") <|> string "no") ; spaces
+            fLJ      <- decimal                                               ; spaces
+            fQQ      <- decimal
+            space `manyTill` (try comment <|> eol)
 
-                return Defaults { non_bonded_function_type = nbfunc
-                                , combination_rule         = combrule
-                                , generate_pairs           = genpairs
-                                , fudgeLJ                  = fLJ
-                                , fudgeQQ                  = fQQ
-                                }
+            return Defaults { non_bonded_function_type = nbfunc
+                            , combination_rule         = combrule
+                            , generate_pairs           = genpairs
+                            , fudgeLJ                  = fLJ
+                            , fudgeQQ                  = fQQ
+                            }
 
 defaults :: Parser Defaults
-defaults = do defaults'
+defaults = do header
               try comment
-              defaults''
+              values
